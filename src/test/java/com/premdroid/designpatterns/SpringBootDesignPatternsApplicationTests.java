9959f0d38bd4eb1f0e00df668c947fc34a7170fa
PREM DHADKAR <premdhadkar@users.noreply.github.com>
2024-09-27 03:26:33 +0530

    Create project structure

---
+++ src/test/java/com/premdroid/designpatterns/SpringBootDesignPatternsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.premdroid.designpatterns;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDesignPatternsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
